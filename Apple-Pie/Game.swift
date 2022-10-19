fb4b1f3b188c4d6e645352eb0af69117c72545d9
Matheus Coelho <matheus_coelho80@yahoo.com>
2022-10-18 21:46:48 -0400

    Initial Commit

---
+++ Apple-Pie/Game.swift
@@ -0,0 +1,35 @@
+//
+//  Game.swift
+//  Apple-Pie
+//
+//  Created by Rafael Medeiros on 10/12/22.
+//
+
+import Foundation
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+            
+        }
+    }
+}
